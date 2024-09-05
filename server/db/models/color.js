2a18b93d408cd1780572192b114ae4b2a6ec09e0
farvastra <farvastra@gmail.com>
2024-09-05 08:42:49 +0100

    first commit

---
+++ server/db/models/color.js
@@ -0,0 +1,27 @@
+
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Color extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  Color.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false, 
+        unique: true, 
+        validate: {
+          notEmpty: true, 
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Color",
+    }
+  );
+  return Color;
+};
